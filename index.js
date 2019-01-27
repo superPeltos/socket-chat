1ccad37cbea25f57e1aca0ffe170eec1831c1ef5
Shadow <tibo.pelletier@gmail.com>
2019-01-28 00:12:22 +0100

    change path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('https://proto-socket-chat.herokuapp.com/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
